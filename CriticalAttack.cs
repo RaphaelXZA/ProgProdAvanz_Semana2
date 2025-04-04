6970ab376cc26024438217725c976a80c08cec20
RaphaelXZA <108958428+RaphaelXZA@users.noreply.github.com>
2025-04-03 21:59:23 -0500

    Interfaz

---
+++ CriticalAttack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgProdAvanz_Semana2
+{
+    internal class CriticalAttack : IAttackBehavior
+    {
+        private int criticalDamage;
+        private int criticalChance = 20;
+        private Random random = new Random();
+
+        public CriticalAttack(int damage)
+        {
+            criticalDamage = damage;
+        }
+
+        public int Attack()
+        {
+            return random.Next(1, 101) <= criticalChance ? criticalDamage * 2 : criticalDamage;
+        }
+    }
+}
---
+++ NormalAttack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgProdAvanz_Semana2
+{
+    internal class NormalAttack : IAttackBehavior
+    {
+        private int normalDamage;
+
+        public NormalAttack(int damage)
+        {
+            normalDamage = damage;
+        }
+
+        public int Attack()
+        {
+            return normalDamage;
+        }
+    }
+}
